3dd5bdd20efb2bfea183c006e2e83712aa3cb3c2
Liveset1 <yousifwilliam12345@outlook.com>
2024-04-20 17:20:50 -0400

    Initial

--- src/main.c
+++ src/main.c
@@ -1,20 +1,21 @@
+#include <windows.h>
 #include <Win32Gui.h>
 int main() {
     WNDCLASSEX wc = {0};
 
     wc.cbSize = sizeof(WNDCLASSEX);
     wc.style = CS_HREDRAW | CS_VREDRAW;
-    wc.lpfnWndProc = (WNDPROC)WndProc;
+    wc.lpfnWndProc = NULL;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
-    wc.hInstance = hInstance;
+    wc.hInstance = GetModuleHandle(NULL);
     wc.hIcon = NULL;
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
     wc.lpszMenuName = NULL;
-    wc.lpszClassName = APPTITLE;
+    wc.lpszClassName = L"Test";
     wc.hIconSm = NULL;
     
-    initialize_window(wc, (Win32Size){ 200, 200, 200, 200, 200, 200}, )
+    initialize_window(wc, (Win32Size){ 200, 200, 200, 200, 200, 200}, NULL, NULL);
     return 0;
 }
